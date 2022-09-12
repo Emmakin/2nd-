6ac07750e3a8e05820ff19bd63e476a337e39ea1
Akindutire Emmanuel <101441304+Emmakin@users.noreply.github.com>
2022-09-13 00:07:01 +0100

    fixed calculation error

--- js/main.js
+++ js/main.js
@@ -68,9 +68,9 @@ Array.from(tipPercentages).forEach((tipPercentageEl) => {
 
 function updateValues({ tipAmountPerPerson, totalAmountPerPerson }) {
     tipAmountPerPersonEl.innerText = 
-      tipAmountPerPerson == Infinity ? 0 : $`$(tipAmountPerPerson.toFixed(2))`;
+      tipAmountPerPerson == Infinity ? 0 : `$${tipAmountPerPerson.toFixed(2)}`;
     totalAmountPerPersonEl.innerText = 
-     totalAmountPerPerson == Infinity ? 0 : $`$(totalAmountPerPerson.toFixed(2))`;
+     totalAmountPerPerson == Infinity ? 0 : `$${totalAmountPerPerson.toFixed(2)}`;
      
   }
 
